6865f862a2c1552dc47057a0c3815b7a67d3de7c
EveBraun <evebraun@example.com>
2024-03-12 20:57:04 +0300

    1

---
+++ users/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+
+# встроенная модель пользователя была в драйверс  from django.contrib.auth.models import User
+
+# второй подход - использование модели абстрактного пользователя
+
+
